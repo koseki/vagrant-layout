eaf15e45ae63faa38616c0d0f37646f3dd15b805
KOSEKI Kengo <koseki@gmail.com>
2014-05-06 13:32:08 +0900

    bundle gem vagrant-clickable

---
+++ lib/vagrant/clickable.rb
@@ -0,0 +1,7 @@
+require "vagrant/clickable/version"
+
+module Vagrant
+  module Clickable
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/clickable/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Clickable
+    VERSION = "0.0.1"
+  end
+end
