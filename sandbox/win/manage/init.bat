5175eb7d8f6fe12f72cdf352930a43c984e7c3de
KOSEKI Kengo <koseki@gmail.com>
2015-01-18 10:06:11 +0900

    Set VBOX_USER_HOME to avoid the problem of non-ASCII characters

--- sandbox/win/manage/init.bat
+++ sandbox/win/manage/init.bat
@@ -1,6 +1,9 @@
-@rem Do not include space in VAGRANT_HOME. Do not use Japanese(CJK?) account name.
-@rem https://github.com/mitchellh/vagrant/issues/4351
+@rem Do not use non-ASCII character and ASCII special character in
+@rem VAGRANT_HOME, VBOX_USER_HOME and your project directory.
+@rem See https://github.com/mitchellh/vagrant/issues/4966
+
 set VAGRANT_HOME=C:\vagrant
+set VBOX_USER_HOME=C:\virtualbox
 
 @rem ssh command required.
 set PATH=C:\Program Files\Git\bin;C:\Program Files (x86)\Git\bin;%PATH%
