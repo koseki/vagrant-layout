80c34e5b3a0878a81c5ba042b124b100b0d7d6b9
KOSEKI Kengo <koseki@gmail.com>
2015-01-21 09:48:00 +0900

    Remove VBOX_USER_HOME setting
    `machinefolder` is the problem. We should not change the global configuration.

--- sandbox/win/manage/init.bat
+++ sandbox/win/manage/init.bat
@@ -1,9 +1,8 @@
 @rem Do not use non-ASCII character and ASCII special character in
-@rem VAGRANT_HOME, VBOX_USER_HOME and your project directory.
+@rem VAGRANT_HOME, VirtualBox machine folder and your project directory.
 @rem See https://github.com/mitchellh/vagrant/issues/4966
 
 set VAGRANT_HOME=C:\vagrant
-set VBOX_USER_HOME=C:\virtualbox
 
 @rem ssh command required.
 set PATH=C:\Program Files\Git\bin;C:\Program Files (x86)\Git\bin;%PATH%
